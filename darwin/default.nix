b4891cfb4a36efdf31ab75bbeade774b7731f5c3
japiirainen <joona.piirainen@gmail.com>
2023-06-20 19:51:48 +0300

    refactor stuff out of flake.nix

---
+++ darwin/default.nix
@@ -0,0 +1,53 @@
+{pkgs, ...}: {
+  documentation.enable = false;
+
+  users.users.japiirainen.home = "/Users/japiirainen";
+
+  programs.zsh.enable = true;
+
+  environment = {
+    shells = with pkgs; [bash zsh];
+
+    loginShell = pkgs.zsh;
+
+    systemPackages = with pkgs; [coreutils];
+
+    systemPath = ["/opt/homebrew/bin"];
+
+    pathsToLink = ["/Applications"];
+  };
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+
+  fonts.fontDir.enable = true; # DANGER
+  fonts.fonts = [(pkgs.nerdfonts.override {fonts = ["Meslo"];})];
+
+  services.nix-daemon.enable = true;
+
+  system.defaults = {
+    finder.AppleShowAllExtensions = true;
+    finder._FXShowPosixPathInTitle = true;
+    dock.autohide = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.InitialKeyRepeat = 1;
+    defaults.NSGlobalDomain.KeyRepeat = 1;
+  };
+
+  homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    masApps = {};
+    casks = ["raycast"];
+    taps = [];
+    brews = [];
+  };
+
+  # backwards compat; don't change
+  system.stateVersion = 4;
+}
--- flake.nix
+++ flake.nix
@@ -16,101 +16,26 @@
     # Tricked out nvim
     pwnvim.url = "github:zmre/pwnvim";
   };
-  outputs = inputs: {
-    darwinConfigurations.jp-mbp = inputs.darwin.lib.darwinSystem {
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    darwin,
+    pwnvim,
+    ...
+  }: {
+    darwinConfigurations.jp-mbp = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      pkgs = import inputs.nixpkgs {system = "aarch64-darwin";};
+      pkgs = import nixpkgs {system = "aarch64-darwin";};
       modules = [
-        ({pkgs, ...}: {
-          # here go the darwin preferences and config items
-          documentation.enable = false;
-          users.users.japiirainen.home = "/Users/japiirainen";
-          programs.zsh.enable = true;
-          environment.shells = [pkgs.bash pkgs.zsh];
-          environment.loginShell = pkgs.zsh;
-          environment.systemPackages = [pkgs.coreutils];
-          environment.systemPath = ["/opt/homebrew/bin"];
-          environment.pathsToLink = ["/Applications"];
-          nix.extraOptions = ''
-            experimental-features = nix-command flakes
-          '';
-          system.keyboard.enableKeyMapping = true;
-          system.keyboard.remapCapsLockToEscape = true;
-          fonts.fontDir.enable = true; # DANGER
-          fonts.fonts = [(pkgs.nerdfonts.override {fonts = ["Meslo"];})];
-          services.nix-daemon.enable = true;
-          system.defaults.finder.AppleShowAllExtensions = true;
-          system.defaults.finder._FXShowPosixPathInTitle = true;
-          system.defaults.dock.autohide = true;
-          system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
-          system.defaults.NSGlobalDomain.InitialKeyRepeat = 1;
-          system.defaults.NSGlobalDomain.KeyRepeat = 1;
-          # backwards compat; don't change
-          system.stateVersion = 4;
-          homebrew = {
-            enable = true;
-            caskArgs.no_quarantine = true;
-            global.brewfile = true;
-            masApps = {};
-            casks = ["raycast" "visual-studio-code"];
-            taps = [];
-            brews = [];
-          };
-        })
-        inputs.home-manager.darwinModules.home-manager
+        ./darwin
+        home-manager.darwinModules.home-manager
         {
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
+            extraSpecialArgs = {inherit pwnvim;};
             users.japiirainen.imports = [
-              ({pkgs, ...}: {
-                # Don't change this when you change package input. Leave it alone.
-                home.stateVersion = "22.11";
-                # specify my home-manager configs
-                home.packages = [
-                  pkgs.ripgrep
-                  pkgs.fd
-                  pkgs.curl
-                  pkgs.less
-                  inputs.pwnvim.packages."aarch64-darwin".default
-                ];
-                home.sessionVariables = {
-                  PAGER = "less";
-                  CLICLOLOR = 1;
-                  EDITOR = "nvim";
-                };
-                programs.bat.enable = true;
-                programs.bat.config.theme = "TwoDark";
-                programs.fzf.enable = true;
-                programs.fzf.enableZshIntegration = true;
-                programs.exa.enable = true;
-
-                programs.git.enable = true;
-                programs.git.userEmail = "joona.piirainen@gmail.com";
-                programs.git.ignores = [".DS_Store"];
-                programs.git.userName = "japiirainen";
-
-                programs.zsh.enable = true;
-                programs.zsh.enableCompletion = true;
-                programs.zsh.enableAutosuggestions = true;
-                programs.zsh.enableSyntaxHighlighting = true;
-                programs.zsh.shellAliases = {ls = "ls --color=auto -F";};
-                programs.starship.enable = true;
-                programs.starship.enableZshIntegration = true;
-                programs.kitty = {
-                  enable = true;
-                };
-                home.file.".inputrc".text = ''
-                  set show-all-if-ambiguous on
-                  set completion-ignore-case on
-                  set mark-directories on
-                  set mark-symlinked-directories on
-                  set match-hidden-files off
-                  set visible-stats on
-                  set keymap vi
-                  set editing-mode vi-insert
-                '';
-              })
+              ./home
             ];
           };
         }
