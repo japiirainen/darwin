7cfdedb662e3c4c478890d7a0e49950662dcba1a
japiirainen <joona.piirainen@gmail.com>
2023-06-22 19:56:20 +0300

    extend neovim config

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,7 +1,83 @@
-{config, ...}:
+{config, lib, pkgs, ...}:
 let
+  inherit (lib) concatStringsSep optional;
   inherit (config.lib.file) mkOutOfStoreSymlink;
   inherit (config.home.user-info) nixConfigDirectory;
+
+  # from malob's configurations
+  mkLuaTableFromList = x: "{" + lib.concatMapStringsSep "," (y: "'${y}'") x + "}";
+  mkNeovimAutocmd = { event, pattern, callback ? "" }: ''
+    vim.api.nvim_create_autocmd(${mkLuaTableFromList event}, {
+      pattern = ${mkLuaTableFromList pattern},
+      callback = ${callback},
+    })'';
+  requireConf = p: "require 'malo.${builtins.replaceStrings [ "." ] [ "-" ] p.pname}'";
+
+  packer =
+    { use
+      # Plugins that this plugin depends on.
+    , deps ? [ ]
+      # Used to manually specify that the plugin shouldn't be loaded at start up.
+    , opt ? false
+      # Whether to load the plugin when using VS Code with `vscode-neovim`.
+    , vscode ? false
+      # Code to run before the plugin is loaded.
+    , setup ? ""
+      # Code to run after the plugin is loaded.
+    , config ? ""
+      # The following all imply lazy-loading and imply `opt = true`.
+      # `FileType`s which load the plugin.
+    , ft ? [ ]
+      # Autocommand events which load the plugin.
+    , event ? [ ]
+    }:
+    let
+      loadFunctionName = "load_${builtins.replaceStrings [ "." "-" ] [ "_" "_" ] use.pname}";
+      autoload = !opt && vscode && ft == [ ] && event == [ ];
+      configFinal =
+        concatStringsSep "\n" (
+          optional (!autoload && !opt) "vim.cmd 'packadd ${use.pname}'"
+          ++ optional (config != "") config
+        );
+    in
+    {
+      plugin = use.overrideAttrs (old: {
+        dependencies = lib.unique (old.dependencies or [ ] ++ deps);
+      });
+      optional = !autoload;
+      type = "lua";
+      config = if (setup == "" && configFinal == "") then null else
+      (
+        concatStringsSep "\n"
+          (
+            [ "\n-- ${use.pname or use.name}" ]
+            ++ optional (setup != "") setup
+
+            # If the plugin isn't always loaded at startup
+            ++ optional (!autoload) (concatStringsSep "\n" (
+              [ "local ${loadFunctionName} = function()" ]
+              ++ optional (!vscode) "if vim.g.vscode == nil then"
+              ++ [ configFinal ]
+              ++ optional (!vscode) "end"
+              ++ [ "end" ]
+              ++ optional (ft == [ ] && event == [ ]) "${loadFunctionName}()"
+              ++ optional (ft != [ ]) (mkNeovimAutocmd {
+                event = [ "FileType" ];
+                pattern = ft;
+                callback = loadFunctionName;
+              })
+              ++ optional (event != [ ]) (mkNeovimAutocmd {
+                inherit event;
+                pattern = [ "*" ];
+                callback = loadFunctionName;
+              })
+            ))
+
+            # If the plugin is always loaded at startup
+            ++ optional (autoload && configFinal != "") configFinal
+          )
+      );
+    };
 in
 {
   programs.neovim.enable = true;
@@ -14,4 +90,27 @@ in
 
   # Load the `init` module from the above configs
   programs.neovim.extraConfig = "lua require('nvim')";
+
+  # Add NodeJs since it's required by some plugins I use.
+  programs.neovim.withNodeJs = true;
+  # Add `penlight` Lua module package since I used in the above configs
+  programs.neovim.extraLuaPackages = ps: [ ps.penlight ];
+
+  # neovim plugins
+  programs.neovim.plugins = with pkgs.vimPlugins; map packer [
+    { use = which-key-nvim; opt = true; }
+    { use = NeoSolarized; opt = true;  }
+    { use = neogit; config = "require'neogit'.setup()"; }
+    {
+      use = telescope-nvim;
+      deps = [
+        nvim-web-devicons
+        telescope-file-browser-nvim
+        telescope-fzf-native-nvim
+        telescope_hoogle
+        telescope-symbols-nvim
+        telescope-zoxide
+      ];
+    }
+  ];
 }
