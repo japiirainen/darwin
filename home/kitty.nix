3819898f0b48868e8d3d2ca0b68e6f8b69c8c868
japiirainen <joona.piirainen@gmail.com>
2023-08-27 20:18:44 +0300

    feat(kitty): increase font_size to 16

--- home/kitty.nix
+++ home/kitty.nix
@@ -4,7 +4,7 @@
   programs.kitty.settings = {
     # font settings
     font_family = "DejaVuSansM Nerd Font Mono";
-    font_size = "14.0";
+    font_size = "16.0";
     adjust_line_height = "100%";
     disable_ligatures = "cursor";
 
