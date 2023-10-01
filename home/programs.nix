d5fa87542bed973eeb89a51016a4e90bb297c498
japiirainen <joona.piirainen@gmail.com>
2023-10-01 18:53:36 +0300

    feat(programs): add agda installation

--- home/programs.nix
+++ home/programs.nix
@@ -29,62 +29,76 @@
     };
   };
 
-  home.packages = lib.attrValues {
-    _ = sp.packages."aarch64-darwin".default;
+  home.packages =
+    lib.attrValues {
+      _ = sp.packages."aarch64-darwin".default;
 
-    inherit (pkgs)
-      tree
-      ripgrep
-      vim
-      fd
-      curl
-      less
-      eza
-      bottom
-      wget
-      neovide
-      dhall
-      ;
-
-    inherit (pkgs)
-      cloc
-      github-copilot-cli
-      typescript
-      nodejs
-      deno
-      bun
-      cbqn
-      ;
-
-    # Nix related tools
-    inherit (pkgs)
-      cachix
-      comma
-      nix-tree
-      nix-update
-      statix
-      ;
-
-    # npm packages
-    inherit (pkgs.nodePackages)
-      prettier
-      ;
-
-    # a minimal python setup with commonly needed libs
-    inherit (pkgs)
-      pyright
-      black
-      pylint
-      ;
-    python311 = pkgs.python311.withPackages
-      (p: with p; [
-        numpy
-        sympy
-        vulture
-        matplotlib
-        fastapi
-        pydantic
-        gunicorn
+      agda = pkgs.agda.withPackages (ps: [
+        (ps.standard-library.overrideAttrs (_: {
+          version = "2.0";
+          src = pkgs.fetchFromGitHub {
+            repo = "agda-stdlib";
+            owner = "agda";
+            rev = "177dc9e983606b653a3c6af2ae2162bbc87882ad";
+            sha256 = "sha256-ovnhL5otoaACpqHZnk/ucivwtEfBQtGRu4/xw4+Ws+c=";
+          };
+        }))
       ]);
-  };
+
+
+      inherit (pkgs)
+        tree
+        ripgrep
+        vim
+        fd
+        curl
+        less
+        eza
+        bottom
+        wget
+        neovide
+        dhall
+        ;
+
+      inherit (pkgs)
+        cloc
+        github-copilot-cli
+        typescript
+        nodejs
+        deno
+        bun
+        cbqn
+        ;
+
+      # Nix related tools
+      inherit (pkgs)
+        cachix
+        comma
+        nix-tree
+        nix-update
+        statix
+        ;
+
+      # npm packages
+      inherit (pkgs.nodePackages)
+        prettier
+        ;
+
+      # a minimal python setup with commonly needed libs
+      inherit (pkgs)
+        pyright
+        black
+        pylint
+        ;
+      python311 = pkgs.python311.withPackages
+        (p: with p; [
+          numpy
+          sympy
+          vulture
+          matplotlib
+          fastapi
+          pydantic
+          gunicorn
+        ]);
+    };
 }
