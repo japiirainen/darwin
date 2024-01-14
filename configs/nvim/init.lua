335b2cbcb22717715fd1d7f02c6d752f98972f99
japiirainen <joona.piirainen@gmail.com>
2024-01-14 16:22:41 +0200

    feat(nvim): add trouble.nvim (diagnostics plugin)

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -260,6 +260,12 @@ require('lazy').setup {
       require('goto-preview').setup {}
     end,
   },
+
+  {
+    -- Nicer diagnostics
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
 }
 
 -- basic vim/neovim settings
@@ -419,6 +425,27 @@ map(
   { desc = '[S]earch [D]iagnostics' }
 )
 map('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+-- trouble diagnostic
+
+map('n', '<leader>xx', function()
+  require('trouble').toggle()
+end, { desc = 'Trouble Toggle' })
+map('n', '<leader>xw', function()
+  require('trouble').toggle 'workspace_diagnostics'
+end, { desc = 'Trouble Workspace Diagnostics' })
+map('n', '<leader>xd', function()
+  require('trouble').toggle 'document_diagnostics'
+end, { desc = 'Trouble Document Diagnostics' })
+map('n', '<leader>xq', function()
+  require('trouble').toggle 'quickfix'
+end, { desc = 'Trouble Quickfix' })
+map('n', '<leader>xl', function()
+  require('trouble').toggle 'loclist'
+end, { desc = 'Trouble Toggle Location List' })
+map('n', 'gR', function()
+  require('trouble').toggle 'lsp_references'
+end, { desc = 'Trouble Lsp References' })
 
 -- Treesitter
 -- See `:help nvim-treesitter`
@@ -855,6 +882,7 @@ require('which-key').register {
   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>i'] = { name = '[I]nput', _ = 'which_key_ignore' },
+  ['<leader>x'] = { name = 'Trouble Diagnostics', _ = 'which_key_ignore' },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
