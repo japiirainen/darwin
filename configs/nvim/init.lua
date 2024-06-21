863ee8c23cb1560aad7f6138757954b4ee866fa3
japiirainen <joona.piirainen@gmail.com>
2024-06-21 19:18:37 +0300

    feat(nvim): add simple scala lsp config

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -265,6 +265,13 @@ require('lazy').setup {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
+
+  {
+    'scalameta/nvim-metals',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 }
 
 -- basic vim/neovim settings
@@ -930,6 +937,19 @@ require('lean').setup {
   lsp3 = { on_attach = on_attach },
   mappings = true,
 }
+
+-- metals
+
+local metals_config = require('metals').bare_config()
+metals_config.on_attach = on_attach
+local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'scala', 'sbt', 'java' },
+  callback = function()
+    require('metals').initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group,
+})
 
 -- nvim-cmp
 local cmp = require 'cmp'
