c794b8970cb59b3657877f7353d1efbd8f0dbaff
japiirainen <joona.piirainen@gmail.com>
2024-07-30 16:51:48 +0200

    chore(nvim): remove unnecessary neogit deps

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -37,12 +37,9 @@ require('lazy').setup {
   {
     'NeogitOrg/neogit',
     dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua', -- optional
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
     },
     config = true,
   },
