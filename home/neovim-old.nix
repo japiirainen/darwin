0975add4050179688d6c6874442026d05f78f0a5
japiirainen <joona.piirainen@gmail.com>
2024-08-29 08:32:22 +0300

    feat(nvim): start implementing new minimal nvim config

--- home/neovim-old.nix
+++ home/neovim-old.nix
@@ -5,64 +5,48 @@ let
 in
 {
   programs.neovim.enable = true;
-  programs.neovim.vimAlias = true;
+
+  # Config and plugins ------------------------------------------------------------------------- {{{
+
+  # Put neovim configuration located in this repository into place in a way that edits to the
+  # configuration don't require rebuilding the `home-manager` environment to take effect.
+  xdg.configFile."nvim/lua/".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/nvim-old";
+
+  # Load the `init` module from the above configs
+  programs.neovim.extraConfig = "lua require('init')";
+
+  programs.neovim.withNodeJs = true;
 
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
 
-  # Put neovim configuration located in this repository into place in a way that edits to the
-  # configuration don't require rebuilding the `home-manager` environment to take effect.
-  xdg.configFile."nvim/lua/".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/nvim";
-
-  # Load the `init` module from the above configs
-  programs.neovim.extraConfig = "lua require('init')";
-
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    (nvim-treesitter.withPlugins (p: [
-      p.bash
-      p.json
-      p.lua
-      p.markdown
-      p.nix
-      p.python
-      p.rust
-      p.zig
-      p.vimdoc
-      p.javascript
-      p.typescript
-      p.tsx
-    ]))
-    nvim-lspconfig
-    conform-nvim
-    neogit
-    fzf-vim
-    which-key-nvim
-  ];
-
   programs.neovim.extraPackages = with pkgs; [
+    neovim-remote
     nodePackages.bash-language-server
     shellcheck
+
     marksman
     nodePackages.typescript-language-server
+    # Uncomment when updating inputs
+    # dhall-lsp-server
+
     deadnix
     nixpkgs-fmt
     statix
     nil
     stylua
+
     nodePackages.vim-language-server
     sumneko-lua-language-server
     vscode-langservers-extracted
     yaml-language-server
     tailwindcss-language-server
     ruff-lsp
+
+    proselint
+    (agda.withPackages (p: [ p.standard-library ]))
+    cornelis
   ];
-
-
-  xdg.configFile."zls.json".text = ''
-    {
-      "enable_build_on_save": true
-    }
-  '';
 }
