804683f40ed7a9838bdccb1e948c88bfa628d2a4
japiirainen <joona.piirainen@gmail.com>
2024-10-13 15:19:35 +0300

    feat(emacs): add simple org-mode config

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -41,6 +41,21 @@
 	:config
 	(evil-commentary-mode))
 
+(use-package org
+  :ensure nil
+  :custom
+  (org-edit-src-content-indentation 2) ;; Set src block automatic indent to 4 instead of 2.
+
+  :hook
+  (org-mode . org-indent-mode) ;; Indent text
+  ;; The following prevents <> from auto-pairing when electric-pair-mode is on.
+  ;; Otherwise, org-tempo is broken when you try to <s TAB...
+  (org-mode . (lambda ()
+                (setq-local electric-pair-inhibit-predicate
+                            `(lambda (c)
+                               (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+  )
+
 ;; Keybindings in org mode
 (use-package evil-org
 	:after evil
