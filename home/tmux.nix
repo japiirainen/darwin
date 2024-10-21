5fb18d50e5030e6745fdd482621a9023e1e0b0f3
japiirainen <joona.piirainen@gmail.com>
2024-10-21 11:31:00 +0300

    feat(tmux): add minimalistic zenburn compatible colorscheme

--- home/tmux.nix
+++ home/tmux.nix
@@ -98,6 +98,11 @@
 
       # don't rename windows automatically
       set-option -g allow-rename off
+
+      # zenburn
+      setw -g clock-mode-colour colour117
+      set -g status-bg colour235
+      set -g status-fg colour248
     '';
   };
 }
