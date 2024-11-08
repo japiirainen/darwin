8fe6ca0b352a38e8cc2dfb13463bd8f2e55e0cd1
japiirainen <joona.piirainen@gmail.com>
2024-11-08 09:33:41 +0200

    feat(helix): change theme

--- home/helix.nix
+++ home/helix.nix
@@ -2,7 +2,7 @@
 {
   programs.helix.enable = true;
   programs.helix.settings = {
-    theme = "catppuccin_macchiato";
+    theme = "catppuccin_mocha";
     editor = {
       line-number = "relative";
       lsp.display-messages = true;
