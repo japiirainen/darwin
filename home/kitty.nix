475e098ceff608ac39b3fdbdb21f6bd5aff6d575
japiirainen <joona.piirainen@gmail.com>
2024-12-19 14:08:16 +0200

    feat(font): change to pragmata pro

--- home/kitty.nix
+++ home/kitty.nix
@@ -5,7 +5,7 @@
   programs.kitty.enable = true;
 
   programs.kitty.settings = {
-    font_family = "Terminess Nerd Font Mono";
+    font_family = "PragmataPro Mono Liga";
     font_size = "18.0";
 
     adjust_line_height = "100%";
