7c57dbdcc3e554700b58023bb89e23aa3b9ebe5b
japiirainen <joona.piirainen@gmail.com>
2025-04-10 14:12:39 +0300

    feat(nvim): add custom snipper for `vitest` suite boilerplate

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -837,6 +837,7 @@ end
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/.config/nvim/lua/snippets' } }
 luasnip.config.setup {}
 
 cmp.setup {
